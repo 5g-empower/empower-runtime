e37fd49ead8fb146ad8dff6b9035911b1a69052e
Roberto Riggio <roberto.riggio@gmail.com>
2016-06-21 10:56:25 +0200

    fix rssi tracker

--- empower/apps/rssitracker/rssitracker.py
+++ empower/apps/rssitracker/rssitracker.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2015, Roberto Riggio
-# All rights reserved.
+# Copyright (c) 2016 Roberto Riggio
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the CREATE-NET nor the
-#      names of its contributors may be used to endorse or promote products
-#      derived from this software without specific prior written permission.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# THIS SOFTWARE IS PROVIDED BY CREATE-NET ''AS IS'' AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL CREATE-NET BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 """Application implementing an rssi tracker."""
 
@@ -52,9 +42,9 @@ class RSSITracker(EmpowerApp):
 
     """
 
-    def __init__(self, tenant, **kwargs):
+    def __init__(self, **kwargs):
         self.__addrs = None
-        EmpowerApp.__init__(self, tenant, **kwargs)
+        EmpowerApp.__init__(self, **kwargs)
         wtpup(tenant_id=self.tenant.tenant_id, callback=self.wtp_up_callback)
 
     @property
@@ -97,13 +87,12 @@ class RSSITracker(EmpowerApp):
 
         for addr in poller.maps.values():
 
-            line = "%f,%s,%.2f,%.2f,%u,%.2f,%.2f\n" % (time.time(),
-                                                       addr['addr'],
-                                                       addr['last_rssi_avg'],
-                                                       addr['last_rssi_std'],
-                                                       addr['last_packets'],
-                                                       addr['ewma_rssi'],
-                                                       addr['sma_rssi'])
+            line = "%f,%s,%.2f,%.2f,%u,%.2f\n" % (time.time(),
+                                                  addr['addr'],
+                                                  addr['last_rssi_avg'],
+                                                  addr['last_rssi_std'],
+                                                  addr['last_packets'],
+                                                  addr['mov_rssi'])
 
             with open(filename, 'a') as file_d:
                 file_d.write(line)
@@ -119,19 +108,18 @@ class RSSITracker(EmpowerApp):
 
         for addr in poller.maps.values():
 
-            line = "%f,%s,%.2f,%.2f,%u,%.2f,%.2f\n" % (time.time(),
-                                                       addr['addr'],
-                                                       addr['last_rssi_avg'],
-                                                       addr['last_rssi_std'],
-                                                       addr['last_packets'],
-                                                       addr['ewma_rssi'],
-                                                       addr['sma_rssi'])
+            line = "%f,%s,%.2f,%.2f,%u,%.2f\n" % (time.time(),
+                                                  addr['addr'],
+                                                  addr['last_rssi_avg'],
+                                                  addr['last_rssi_std'],
+                                                  addr['last_packets'],
+                                                  addr['mov_rssi'])
 
             with open(filename, 'a') as file_d:
                 file_d.write(line)
 
 
-def launch(tenant, addrs=DEFAULT_ADDRESS, period=DEFAULT_PERIOD):
+def launch(tenant_id, addrs=DEFAULT_ADDRESS, every=DEFAULT_PERIOD):
     """ Initialize the module. """
 
-    return RSSITracker(tenant, addrs=addrs, every=period)
+    return RSSITracker(tenant_id=tenant_id, addrs=addrs, every=every)
