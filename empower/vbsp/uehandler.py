a6ab52f220f7b4e8a0d2b24ec7be99f2cdb4cd0b
herlesupreeth <herlesupreeth@gmail.com>
2016-11-30 17:12:22 +0100

    Removed redundant code

--- empower/vbsp/uehandler.py
+++ empower/vbsp/uehandler.py
@@ -24,44 +24,6 @@ from empower.main import RUNTIME
 import empower.logger
 LOG = empower.logger.get_logger()
 
-
-class UEHandler(EmpowerAPIHandlerAdminUsers):
-    """UE handler. Used to view UEs in a VBS (controller-wide)."""
-
-    HANDLERS = [r"/api/v1/ues/?",
-                r"/api/v1/ues/([a-zA-Z0-9:]*)/?"]
-
-    def get(self, *args, **kwargs):
-        """ Get all UEs or just the specified one.
-
-        Args:
-            vbs_id: the vbs identifier
-            rnti: the radio network temporary identifier
-
-        Example URLs:
-            GET /api/v1/ues
-            GET /api/v1/ues/11:22:33:44:55:66
-        """
-
-        try:
-
-            if len(args) > 1:
-                raise ValueError("Invalid URL")
-
-            if len(args) == 0:
-                self.write_as_json(RUNTIME.ues.values())
-            else:
-                ue_addr = EtherAddress(args[0])
-                print(RUNTIME.ues.keys())
-                self.write_as_json(RUNTIME.ues[ue_addr])
-
-        except KeyError as ex:
-           self.send_error(404, message=ex)
-        except ValueError as ex:
-            self.send_error(400, message=ex)
-
-        self.set_status(200, None)
-
 class UEHandler(EmpowerAPIHandlerAdminUsers):
     """UE handler. Used to view UEs attached to a VBS (controller-wide)."""
 
