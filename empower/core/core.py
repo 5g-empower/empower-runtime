e599b453b122d1f517f65a3d820eef4a94dfe8ad
herlesupreeth <herlesupreeth@gmail.com>
2016-12-06 16:54:32 +0100

    Sent UE leave message first and then removed the UE instance from tenants

--- empower/core/core.py
+++ empower/core/core.py
@@ -629,14 +629,14 @@ class EmpowerRuntime(object):
 
         ue = self.ues[ue_addr]
 
-        # removing UE from tenant, need first to look for right tenant
-        if ue.addr in ue.tenant.ues:
-            LOG.info("Removing %s from tenant %u", ue.addr, ue.plmn_id)
-            del ue.tenant.ues[ue.addr]
-
         # Raise UE leave event
         from empower.vbsp.vbspserver import VBSPServer
         vbsp_server = self.components[VBSPServer.__module__]
         vbsp_server.send_ue_leave_message_to_self(ue)
+
+        # removing UE from tenant, need first to look for right tenant
+        if ue.addr in ue.tenant.ues:
+            LOG.info("Removing %s from tenant %u", ue.addr, ue.plmn_id)
+            del ue.tenant.ues[ue.addr]
 
         del self.ues[ue.addr]
