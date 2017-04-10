a52427d5fc22a871ea3829a5ec3a5e2b8c25a069
Roberto Riggio <rriggio@Robertos-MacBook-Pro.local>
2017-04-10 17:15:08 +0200

    fix ue remove code

--- empower/core/core.py
+++ empower/core/core.py
@@ -704,15 +704,16 @@ class EmpowerRuntime(object):
 
         ue = self.ues[ue_addr]
 
-        # Raise UE leave event
-        from empower.vbsp.vbspserver import VBSPServer
-        vbsp_server = self.components[VBSPServer.__module__]
         if ue.tenant:
-            vbsp_server.send_ue_leave_message_to_self(ue)
 
             # removing UE from tenant, need first to look for right tenant
             if ue.addr in ue.tenant.ues:
                 LOG.info("Removing %s from tenant %u", ue.addr, ue.plmn_id)
                 del ue.tenant.ues[ue.addr]
+
+            # Raise UE leave event
+            from empower.vbsp.vbspserver import VBSPServer
+            vbsp_server = self.components[VBSPServer.__module__]
+            vbsp_server.send_ue_leave_message_to_self(ue)
 
         del self.ues[ue.addr]
