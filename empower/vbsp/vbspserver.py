4bddf5befb70de7e5fae16362a17d721adbfd3e5
Roberto Riggio <rriggio@MacBook-Pro-3.local>
2017-11-06 15:31:51 +0100

    minor bug

--- empower/vbsp/vbspserver.py
+++ empower/vbsp/vbspserver.py
@@ -140,7 +140,7 @@ class VBSPServer(PNFPServer, TCPServer):
     def send_ue_join_message_to_self(self, ue):
         """Send an UE_JOIN message to self."""
 
-        self.log.info("UE JOIN %u (%u)", ue.imsi, ue.plmn_id)
+        self.log.info("UE JOIN %u (%s)", ue.imsi, ue.plmn_id)
         for handler in self.pt_types_handlers[PT_UE_JOIN]:
             handler(ue)
 
