5473248d83117d9e0c0eb0e8369f664220e4d1a8
Roberto Riggio <rriggio@MacBook-Pro-4.local>
2018-02-06 16:54:09 +0100

    remove some ovs ports ids

--- empower/core/virtualport.py
+++ empower/core/virtualport.py
@@ -29,10 +29,10 @@ from empower.main import RUNTIME
 class VirtualPort:
     """Virtual port."""
 
-    def __init__(self, virtual_port_id, ports=None):
+    def __init__(self, virtual_port_id):
 
         self.virtual_port_id = virtual_port_id
-        self.ports = []
+        self.poas = []
         self.next = VirtualPortProp()
 
     def clear(self):
@@ -48,7 +48,7 @@ class VirtualPort:
         """ Return a JSON-serializable dictionary representing the Port """
 
         return {'virtual_port_id': self.virtual_port_id,
-                'ports': self.ports}
+                'poas': self.poas}
 
     def __hash__(self):
 
@@ -61,9 +61,9 @@ class VirtualPort:
 
     def __repr__(self):
 
-        ports = [str(x) for x in self.ports]
-        return "virtual_port %u ports [%s]" % \
-            (self.virtual_port_id, ", ".join(ports))
+        poas = [str(x) for x in self.poas]
+        return "virtual_port %u poas [%s]" % \
+            (self.virtual_port_id, ", ".join(poas))
 
 
 class VirtualPortProp(dict):
@@ -102,12 +102,12 @@ class VirtualPortProp(dict):
         intent_server = RUNTIME.components[IntentServer.__module__]
 
         # Send intents
-        for port in self.ports:
+        for port in self.poas:
 
             # set/update intent
             intent = {'version': '1.0',
-                      'ttp_dpid': value.dpid,
-                      'ttp_port': value.ovs_port_id,
+                      'ttp_dpid': value.poas[0].dpid,
+                      'ttp_port': value.poas[0].port_id,
                       'stp_dpid': port.dpid,
                       'stp_port': port.port_id,
                       'match': ofmatch_s2d(key)}
