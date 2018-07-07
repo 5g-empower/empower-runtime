2d7cf031a2bccf247a1335bb73906043ecce61a6
Roberto Riggio <rriggio@MacBook-Pro-4.station>
2018-07-07 18:04:22 +0200

    update wifi apps

--- empower/apps/survey/survey.py
+++ empower/apps/survey/survey.py
@@ -20,52 +20,29 @@
 from empower.core.app import EmpowerApp
 from empower.core.app import DEFAULT_PERIOD
 from empower.core.resourcepool import BANDS
-from empower.datatypes.etheraddress import EtherAddress
-
-DEFAULT_ADDRESS = "ff:ff:ff:ff:ff:ff"
 
 
 class Survey(EmpowerApp):
     """Survey App.
 
     Command Line Parameters:
-
         tenant_id: tenant id
-        addr: the address to be tracked (optional, default ff:ff:ff:ff:ff:ff)
         every: loop period in ms (optional, default 5000ms)
 
     Example:
-
         ./empower-runtime.py apps.survey.survey \
             --tenant_id=52313ecb-9d00-4b7d-b873-b55d3d9ada26
     """
 
     def __init__(self, **kwargs):
-        self.__addr = None
         super().__init__(**kwargs)
         self.links = {}
-        self.wtpup(callback=self.wtp_up_callback)
 
-    @property
-    def addr(self):
-        """Return addr."""
-
-        return self.__addr
-
-    @addr.setter
-    def addr(self, value):
-        """Set addr."""
-
-        self.__addr = EtherAddress(value)
-
-    def wtp_up_callback(self, wtp):
+    def wtp_up(self, wtp):
         """New WTP."""
 
         for block in wtp.supports:
-            self.summary(addr=self.addr,
-                         block=block,
-                         period=1000,
-                         callback=self.summary_callback)
+            self.summary(block=block, callback=self.summary_callback)
 
     def to_dict(self):
         """ Return a JSON-serializable dictionary representing the Summary """
@@ -121,7 +98,7 @@ class Survey(EmpowerApp):
                 file_d.write(line)
 
 
-def launch(tenant_id, addr=DEFAULT_ADDRESS, every=DEFAULT_PERIOD):
+def launch(tenant_id, every=DEFAULT_PERIOD):
     """ Initialize the module. """
 
-    return Survey(tenant_id=tenant_id, addr=addr, every=every)
+    return Survey(tenant_id=tenant_id, every=every)
