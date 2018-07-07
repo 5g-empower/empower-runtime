aae0aff7cfa8e68589e66a94342a6e11279cd5d3
Roberto Riggio <rriggio@MacBook-Pro-4.station>
2018-07-07 17:43:17 +0200

    split get/set modules

---
+++ empower/lvnfp/lvnf_set/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2016 Roberto Riggio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""LVNF Element Management System."""
+
+PT_LVNF_GET_REQUEST = "lvnf_get_request"
+PT_LVNF_GET_RESPONSE = "lvnf_get_response"
+PT_LVNF_SET_REQUEST = "lvnf_set_request"
+PT_LVNF_SET_RESPONSE = "lvnf_set_response"
