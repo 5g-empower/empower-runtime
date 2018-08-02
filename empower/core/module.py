fc3b8c8d3bccb03a8fcd40eb09f26b1096b060f0
Roberto Riggio <roberto.riggio@gmail.com>
2018-08-02 13:03:16 +0200

    move non tenant code from module to service

--- empower/core/module.py
+++ empower/core/module.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2016 Roberto Riggio
+# Copyright (c) 2018 Roberto Riggio
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,51 +17,17 @@
 
 """EmPOWER Primitive Base Class."""
 
-import json
-import types
-import xmlrpc.client
-
-from multiprocessing.pool import ThreadPool
-
 import tornado.web
 import tornado.httpserver
-from tornado.ioloop import IOLoop
 
-import empower.logger
-
+from empower.core.utils import get_xid
 from empower.core.service import ServiceWorker
-from empower.core.jsonserializer import EmpowerEncoder
+from empower.core.service import Service
 
 from empower.main import RUNTIME
 
 
-_WORKERS = ThreadPool(10)
-
-
-def exec_xmlrpc(callback, args=()):
-    """Execute XML-RPC call."""
-
-    proxy = xmlrpc.client.ServerProxy(callback[0])
-    func = getattr(proxy, callback[1])
-    run_background(func, on_complete, args)
-
-
-def run_background(func, callback, args=(), kwds=None):
-    """Run callback in background."""
-
-    def _callback(result):
-        IOLoop.instance().add_callback(lambda: callback(result))
-
-    _WORKERS.apply_async(func, args, kwds, _callback)
-
-
-def on_complete(_):
-    """XML RPC Callback."""
-
-    pass
-
-
-class Module:
+class Module(Service):
     """Module object.
 
     Attributes:
@@ -77,55 +43,9 @@ class Module:
 
     def __init__(self):
 
-        self.module_id = 0
-        self.module_type = None
-        self.worker = None
+        super().__init__()
+
         self.__tenant_id = None
-        self.__callback = None
-        self.__periodic = None
-        self.log = empower.logger.get_logger()
-
-    def unload(self):
-        """Remove this module."""
-
-        self.worker.remove_module(self.module_id)
-
-    def handle_callback(self, serializable):
-        """Handle an module callback.
-
-        Args:
-            serializable, an object implementing the to_dict() method
-
-        Returns:
-            None
-        """
-
-        # call callback if defined
-        if not self.callback:
-            return
-
-        callback = self.callback
-
-        try:
-
-            as_dict = serializable.to_dict()
-            as_json = json.dumps(as_dict, cls=EmpowerEncoder)
-
-            if isinstance(callback, (types.FunctionType, types.MethodType)):
-
-                callback(serializable)
-
-            elif isinstance(callback, list) and len(callback) == 2:
-
-                exec_xmlrpc(callback, (as_json, ))
-
-            else:
-
-                raise TypeError("Invalid callback type")
-
-        except Exception as ex:
-
-            self.log.exception(ex)
 
     @property
     def tenant_id(self):
@@ -149,46 +69,6 @@ class Module:
 
         return out
 
-    def as_json(self):
-        """Return a JSON representation of the object."""
-
-        return json.dumps(self.to_dict(), cls=EmpowerEncoder, indent=4)
-
-    @property
-    def callback(self):
-        """ Return this triger callback. """
-
-        return self.__callback
-
-    @callback.setter
-    def callback(self, callback=None):
-        """Register callback function.
-
-        The callback is generated when the condition is verified. Callback can
-        be either a reference to a fucntion or an tuple whose first entry is
-        the URL of a remote xmlrpc server and the second entry is the method
-        to be called.
-        """
-
-        if not callback:
-            self.__callback = None
-            return
-
-        if isinstance(callback, (types.FunctionType, types.MethodType)):
-
-            self.__callback = callback
-
-        elif isinstance(callback, list) and len(callback) == 2:
-
-            self.__callback = callback
-
-        else:
-
-            raise TypeError("Invalid callback type")
-
-    def __str__(self):
-        return "Module %u type %s" % (self.module_id, self.module_type)
-
     def __hash__(self):
         return hash(str(self.tenant_id) + str(self.module_id))
 
@@ -196,33 +76,9 @@ class Module:
 
         if isinstance(other, Module):
             return self.module_type == other.module_type and \
-                self.tenant_id == other.tenant_id and \
-                self.callback == other.callback
+                self.tenant_id == other.tenant_id
 
         return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def start(self):
-        """Start worker."""
-
-        self.run_once()
-
-    def stop(self):
-        """Stop worker."""
-
-        pass
-
-    def run_once(self):
-        """Period task."""
-
-        pass
-
-    def handle_response(self, response):
-        """Stub handle response method."""
-
-        pass
 
 
 class ModuleSingle(Module):
@@ -297,8 +153,7 @@ class ModulePeriodic(Module):
         if isinstance(other, Module):
             return self.module_type == other.module_type and \
                 self.tenant_id == other.tenant_id and \
-                self.every == other.every and \
-                self.callback == other.callback
+                self.every == other.every
 
         return False
 
@@ -309,29 +164,16 @@ class ModuleWorker(ServiceWorker):
     Keeps track of the currently defined modules for each tenant
 
     Attributes:
-        module_id: Next module id
         modules: dictionary of modules currently active in this tenant
     """
 
     def __init__(self, server, module, pt_type, pt_packet):
 
-        super().__init__(every=-1)
+        super().__init__(server, pt_type, pt_packet)
 
         self.__module_id = 0
         self.modules = {}
-        self.pt_type = pt_type
-        self.pt_packet = pt_packet
         self.module = module
-        self.pnfp_server = RUNTIME.components[server]
-
-        self.pnfp_server.register_message(self.pt_type,
-                                          self.pt_packet,
-                                          self.handle_packet)
-
-    def handle_packet(self, pnfdev, message):
-        """Handle response message."""
-
-        pass
 
     @property
     def module_id(self):
