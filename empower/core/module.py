8ebb26a7a4e634dd8642b152a22cfa5a2158b51f
Roberto Riggio <roberto.riggio@gmail.com>
2018-09-23 17:13:38 +0200

    merge service into module since serve will be used for system wide apps

--- empower/core/module.py
+++ empower/core/module.py
@@ -17,16 +17,50 @@
 
 """EmPOWER Primitive Base Class."""
 
+import json
+import types
+import xmlrpc.client
+
+from multiprocessing.pool import ThreadPool
+
 import tornado.web
 import tornado.httpserver
 
-from empower.core.service import ServiceWorker
-from empower.core.service import Service
+from tornado.ioloop import IOLoop
 
+import empower.logger
+
+from empower.core.jsonserializer import EmpowerEncoder
 from empower.main import RUNTIME
 
 
-class Module(Service):
+_WORKERS = ThreadPool(10)
+
+
+def exec_xmlrpc(callback, args=()):
+    """Execute XML-RPC call."""
+
+    proxy = xmlrpc.client.ServerProxy(callback[0])
+    func = getattr(proxy, callback[1])
+    run_background(func, on_complete, args)
+
+
+def run_background(func, callback, args=(), kwds=None):
+    """Run callback in background."""
+
+    def _callback(result):
+        IOLoop.instance().add_callback(lambda: callback(result))
+
+    _WORKERS.apply_async(func, args, kwds, _callback)
+
+
+def on_complete(_):
+    """XML RPC Callback."""
+
+    pass
+
+
+class Module:
     """Module object.
 
     Attributes:
@@ -42,9 +76,112 @@ class Module(Service):
 
     def __init__(self):
 
-        super().__init__()
-
         self.__tenant_id = None
+        self.module_id = 0
+        self.module_type = None
+        self.worker = None
+        self.__callback = None
+        self.__periodic = None
+        self.log = empower.logger.get_logger()
+
+    def unload(self):
+        """Remove this module."""
+
+        self.worker.remove_module(self.module_id)
+
+    def handle_callback(self, serializable):
+        """Handle an module callback.
+
+        Args:
+            serializable, an object implementing the to_dict() method
+
+        Returns:
+            None
+        """
+
+        # call callback if defined
+        if not self.callback:
+            return
+
+        callback = self.callback
+
+        try:
+
+            as_dict = serializable.to_dict()
+            as_json = json.dumps(as_dict, cls=EmpowerEncoder)
+
+            if isinstance(callback, (types.FunctionType, types.MethodType)):
+
+                callback(serializable)
+
+            elif isinstance(callback, list) and len(callback) == 2:
+
+                exec_xmlrpc(callback, (as_json, ))
+
+            else:
+
+                raise TypeError("Invalid callback type")
+
+        except Exception as ex:
+
+            self.log.exception(ex)
+
+    def as_json(self):
+        """Return a JSON representation of the object."""
+
+        return json.dumps(self.to_dict(), cls=EmpowerEncoder, indent=4)
+
+    @property
+    def callback(self):
+        """ Return this triger callback. """
+
+        return self.__callback
+
+    @callback.setter
+    def callback(self, callback=None):
+        """Register callback function.
+
+        The callback is generated when the condition is verified. Callback can
+        be either a reference to a fucntion or an tuple whose first entry is
+        the URL of a remote xmlrpc server and the second entry is the method
+        to be called.
+        """
+
+        if not callback:
+            self.__callback = None
+            return
+
+        if isinstance(callback, (types.FunctionType, types.MethodType)):
+
+            self.__callback = callback
+
+        elif isinstance(callback, list) and len(callback) == 2:
+
+            self.__callback = callback
+
+        else:
+
+            raise TypeError("Invalid callback type")
+
+    def start(self):
+        """Start worker."""
+
+        self.run_once()
+
+    def stop(self):
+        """Stop worker."""
+
+        pass
+
+    def run_once(self):
+        """Period task."""
+
+        pass
+
+    def handle_response(self, response):
+        """Stub handle response method."""
+
+        pass
 
     @property
     def tenant_id(self):
@@ -79,6 +216,11 @@ class Module(Service):
 
         return False
 
+    def __str__(self):
+        return "Module %u type %s" % (self.module_id, self.module_type)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 class ModuleSingle(Module):
     """Module Single object."""
@@ -157,7 +299,7 @@ class ModulePeriodic(Module):
         return False
 
 
-class ModuleWorker(ServiceWorker):
+class ModuleWorker:
     """Module worker.
 
     Keeps track of the currently defined modules for each tenant
@@ -168,11 +310,19 @@ class ModuleWorker(ServiceWorker):
 
     def __init__(self, server, module, pt_type, pt_packet):
 
-        super().__init__(server, pt_type, pt_packet)
-
         self.__module_id = 0
         self.modules = {}
         self.module = module
+
+        self.pt_type = pt_type
+        self.pt_packet = pt_packet
+        self.pnfp_server = RUNTIME.components[server]
+
+        self.pnfp_server.register_message(self.pt_type,
+                                          self.pt_packet,
+                                          self.handle_packet)
+
+        self.log = empower.logger.get_logger()
 
     @property
     def module_id(self):
@@ -260,3 +410,8 @@ class ModuleWorker(ServiceWorker):
         module.stop()
 
         del self.modules[module_id]
+
+    def handle_packet(self, pnfdev, message):
+        """Handle response message."""
+
+        pass
