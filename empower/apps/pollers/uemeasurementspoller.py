f5b1503de3e0193f8f9dbefb4a90bee559efc76d
EstefaniaCC <estefi.coronado@gmail.com>
2018-10-09 16:05:47 +0200

    Fixed bugs in the ue measruements and mac reports

--- empower/apps/pollers/uemeasurementspoller.py
+++ empower/apps/pollers/uemeasurementspoller.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""RRC Statistics Poller Apps."""
+"""UE Statistics Poller Apps."""
 
 from empower.core.app import EmpowerApp
 
