2dd00b989f10548d717b5ad2ec0c28c9e75121e1
Roberto Riggio <rriggio@robertos-mbp.fbkeduroam.it>
2019-10-30 10:20:33 +0100

    Add back datatable libs for js.

--- empower/managers/apimanager/static/js/empower-wtps.js
+++ empower/managers/apimanager/static/js/empower-wtps.js
@@ -1,3 +1,8 @@
 $('#devices').removeClass('collapse');
 $('#collapseTwo').addClass('show');
 $('#devices_wtps').addClass('active');
+
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable();
+});
