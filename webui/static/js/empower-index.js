93359592ad10ddfb5b46bf48edc40d928a068b25
Roberto Riggio <rriggio@Robertos-MBP.station>
2019-11-10 21:17:25 +0100

    Use four cols

--- webui/static/js/empower-index.js
+++ webui/static/js/empower-index.js
@@ -41,7 +41,7 @@ function refresh_projects() {
 
       }
 
-      projects += '<div class="col-xl-5 col-md-6 mb-4"><div class="card border-left-primary shadow h-100 py-2"><div class="card-body"><div class="row no-gutters align-items-center"><div class="col mr-2"><div class="text-xs font-weight-bold text-primary text-uppercase mb-1">' + key + '</div><div class="h5 mb-0 font-weight-bold text-gray-800">' + val['desc'] + '</div></div><div class="col-auto">' + cmd + '</div></div></div></div></div>'
+      projects += '<div class="col-xl-4 col-md-6 mb-4"><div class="card border-left-primary shadow h-100 py-2"><div class="card-body"><div class="row no-gutters align-items-center"><div class="col mr-2"><div class="text-xs font-weight-bold text-primary text-uppercase mb-1">' + key + '</div><div class="h5 mb-0 font-weight-bold text-gray-800">' + val['desc'] + '</div></div><div class="col-auto">' + cmd + '</div></div></div></div></div>'
 
     });
 
