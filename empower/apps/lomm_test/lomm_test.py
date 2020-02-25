3f7ce05bedbc38aa2b602d10ac5db3c79a7cd992
ccosta <ccosta@fbk.eu>
2020-02-25 10:42:12 +0100

    Added copyrigth information

--- empower/apps/lomm_test/lomm_test.py
+++ empower/apps/lomm_test/lomm_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2019, CREATE-NET FBK, Trento, Italy
+# Copyright (c) 2020 Cristina Costa
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,12 +31,10 @@ from   empower.core.app import EApp
 from   empower.core.app import EVERY
         
 __author__       = "Cristina E. Costa"
-__copyright__    = "Copyright 2019, FBK (https://www.fbk.eu)"
 __credits__      = ["Cristina E. Costa"]
 __license__      = "Apache License, Version 2.0"
 __version__      = "1.0.0"
 __maintainer__   = "Cristina E. Costa"
-__contributors__ = ""
 __email__        = "ccosta@fbk.eu"
 __status__       = "Dev"
 
--- empower/managers/lommmanager/lnsdp/lns.py
+++ empower/managers/lommmanager/lnsdp/lns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2019, CREATE-NET FBK, Trento, Italy
+# Copyright (c) 2020 Cristina Costa
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
