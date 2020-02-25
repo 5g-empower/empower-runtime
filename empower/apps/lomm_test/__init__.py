8c7e8cf0abb293b2a889a4f15cca50430429b333
ccosta <ccosta@fbk.eu>
2020-02-25 10:20:11 +0100

    LoMM test app (lomm_test) added

---
+++ empower/apps/lomm_test/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2019, CREATE-NET FBK, Trento, Italy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""LoMM Test app."""
+
+from empower.core.app import EVERY
+
+__author__       = "Cristina E. Costa"
+__copyright__    = "Copyright 2019, FBK (https://www.fbk.eu)"
+__credits__      = ["Cristina E. Costa"]
+__license__      = "Apache License, Version 2.0"
+__version__      = "1.0.0"
+__maintainer__   = "Cristina E. Costa"
+__contributors__ = ""
+__email__        = "ccosta@fbk.eu"
+__status__       = "Dev"
+
+MANIFEST = {
+    "label": "LoMMTest",
+    "desc":  "The LoMM Test App, prints LoMM events data on screen",
+    "params": {
+    }
+}
