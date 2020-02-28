f1dbb6d4a7f38b1e8930365e591014553cd4139f
ccosta <ccosta@fbk.eu>
2020-02-28 08:35:45 +0100

    Added LoMMApp class; LoMM Test app update

--- empower/apps/lomm_test/__init__.py
+++ empower/apps/lomm_test/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2020 Cristina Costa
+# Copyright (c) 2020 Fondazione Bruno Kessler
+# Author(s): Cristina Costa (ccosta@fbk.eu)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,14 +18,6 @@
 """LoMM Test app."""
 
 from empower.core.app import EVERY
-
-__author__       = "Cristina E. Costa"
-__credits__      = ["Cristina E. Costa"]
-__license__      = "Apache License, Version 2.0"
-__version__      = "1.0.0"
-__maintainer__   = "Cristina E. Costa"
-__email__        = "ccosta@fbk.eu"
-__status__       = "Dev"
 
 MANIFEST = {
     "label": "LoMMTest",
