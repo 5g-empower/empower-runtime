f962288195a84c3e4612f6175b06537da75162f8
ingdestino <afrancescon@fbk.eu>
2020-05-18 15:24:43 +0200

    Fixed missing reference to core functions

--- webui/static/js/pages/empower-workers.js
+++ webui/static/js/pages/empower-workers.js
@@ -79,6 +79,7 @@ function clear_active_worker_list(){
 }
 
 function alter_modal(type, key, descriptor){
+  let cf = __EMPOWER_WEBUI.CORE_FUNCTIONS
   console.log("ALTER MODAL",type, key, descriptor)
   let modal_hacker = new WEBUI_Modal_Hacker_Worker("worker_modal")
 
