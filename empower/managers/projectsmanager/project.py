857b80d1a86b4ceaec171e9b22258ee65c262c8c
Roberto Riggio <robertoriggio@Robertos-MBP.station>
2021-01-02 15:03:04 +0100

    Remove duplicate code

--- empower/managers/projectsmanager/project.py
+++ empower/managers/projectsmanager/project.py
@@ -431,42 +431,6 @@ class EmpowerProject(Env):
         self.save()
         self.refresh_from_db()
 
-    @property
-    def users(self):
-        """Return the UEs."""
-
-        if not self.lte_props:
-            return {}
-
-        users = {k: v for k, v in srv_or_die("vbspmanager").users.items()
-                 if v.plmnid == self.lte_props.plmnid}
-
-        return users
-
-    @property
-    def lvaps(self):
-        """Return the LVAPs."""
-
-        if not self.wifi_props:
-            return {}
-
-        lvaps = {k: v for k, v in srv_or_die("lvappmanager").lvaps.items()
-                 if v.ssid == self.wifi_props.ssid}
-
-        return lvaps
-
-    @property
-    def vaps(self):
-        """Return the VAPs."""
-
-        if not self.wifi_props:
-            return {}
-
-        vaps = {k: v for k, v in srv_or_die("lvappmanager").vaps.items()
-                if v.ssid == self.wifi_props.ssid}
-
-        return vaps
-
     def to_dict(self):
         """Return JSON-serializable representation of the object."""
 
